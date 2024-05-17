5d75b525ab7dff6503641d497c2c592f1240edb5
Dunyadj <dunya.jadallah18@gmail.com>
2024-05-17 12:40:25 -0400

    Update your_name24.js

--- your_name24.js
+++ your_name24.js
@@ -1,10 +1,17 @@
 // Initialize Leaflet map
-var map = L.map('nymap').setView([40.62112, -74.02650], 13); // Set center to New York City and zoom level
+var map = L.map('nymap').setView([40.681801312931455, -73.9571896025164], 13); // Set center to New York City and zoom level
 
 // Add a tile layer to the map (for example, OpenStreetMap)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
+
+
+
+
+
+// Add a marker to the map for New York City
+var luigismarker = L.marker([40.62112, -74.02650]).addTo(map);
 
 // Add a popup to the marker
 luigismarker.bindPopup(`
