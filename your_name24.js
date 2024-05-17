c7d7beeb122c3b8111aab593e9d5012360777265
Dunyadj <dunya.jadallah18@gmail.com>
2024-05-17 12:33:18 -0400

    Update your_name24.js

--- your_name24.js
+++ your_name24.js
@@ -6,8 +6,8 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-// Add a marker to the map for New York City
-var marker = L.marker([40.681801312931455, -73.9571896025164]).addTo(map);
-
 // Add a popup to the marker
-marker.bindPopup("<b>Pizzawagon Pizza</b><br>This is a Leaflet map.").openPopup();
+luigismarker.bindPopup(`
+    <b>Luigi's Pizza</b><br>Best pizza on Dekalb Ave<br>
+    <img src="https://aurashktest.github.io/aurashktest/images/luigis.png" style="width: 100px; height: auto;">
+`).openPopup();
